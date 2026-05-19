3d042bbe2f31ba4e43bb2538506b802ddb9dd31b
Caden Croft <cadencroft@gmail.com>
2026-05-18 23:13:46 -0400

    Added color changing functionality

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-.
+document.querySelector(".red").addEventListener("click", function(){
+    document.body.style.backgroundColor="rgb(255, 43, 43)"});
+
+document.querySelector(".orange").addEventListener("click", function(){
+    document.body.style.backgroundColor="orange"});
+
+document.querySelector(".yellow").addEventListener("click", function(){
+    document.body.style.backgroundColor="yellow"});
+
+document.querySelector(".green").addEventListener("click", function(){
+    document.body.style.backgroundColor="rgb(51, 216, 51)"});
+
+document.querySelector(".blue").addEventListener("click", function(){
+    document.body.style.backgroundColor="rgb(72, 145, 255)"});
+
+document.querySelector(".purple").addEventListener("click", function(){
+    document.body.style.backgroundColor="rgb(236, 116, 236)"});
+
+document.querySelector(".reset-btn").addEventListener("click", function(){
+    document.body.style.backgroundColor="white"});
